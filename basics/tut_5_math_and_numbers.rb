1779bb7b1a358dcdac43b25cce12c48af04fde51
Ruby Hoshino <sarthakpanda784@outlook.com>
2025-12-13 16:45:17 +0000

    Update tut_5_math_and_numbers.rb

---
+++ basics/tut_5_math_and_numbers.rb
@@ -0,0 +1,15 @@
+puts 5 + 9
+puts 2 ** 3
+puts 10 % 3
+num = 20
+puts ("my fav num " + num.to_s)
+num = -20
+puts num.abs()
+num = 20.23
+puts num.round()
+puts num.ceil()
+puts num.floor()
+puts Math.sqrt(36)
+puts Math.log(1)
+puts 10 / 7
+puts 10 / 7.0
