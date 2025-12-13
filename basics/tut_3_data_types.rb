571f0b4af0c35143a483bb50fa0125f548ce7e90
Ruby Hoshino <sarthakpanda784@outlook.com>
2025-12-13 16:31:24 +0000

    Tut 2 to 4

---
+++ basics/tut_3_data_types.rb
@@ -0,0 +1,7 @@
+name = 'Mike'
+occupation = 'progammer'
+age = 75
+gpa = 3.25
+ismale = true
+istall = false
+flaws = nil
---
+++ basics/tut_4_working_with_strings.rb
@@ -0,0 +1,12 @@
+puts "Giraffe\"\'\n Academy"
+phrase = "     Giraffe Academy    "
+puts phrase.upcase()
+puts phrase.downcase()
+puts phrase.strip()
+phrase = "Giraffe Academy"
+puts phrase.length()
+puts phrase.include? "Academy"
+puts phrase[0]
+puts phrase[0, 3]
+puts phrase.index("A")
+puts phrase.index("ffe")
