ab3585c04d850c11de4a2955f1419627b56ae067
Ruby Hoshino <sarthakpanda784@outlook.com>
2025-12-14 05:24:45 +0000

    Update tut_10_hashes.rb

---
+++ basics/tut_10_hashes.rb
@@ -0,0 +1,11 @@
+states = {
+  "Pennsy lvania" => "PA",
+  "New York" => "NY",
+  "Oregon" => "OR",
+  :Pennsylvania => "PA",
+  1 => "PA",
+}
+puts states
+puts states["New York"]
+puts states[1]
+puts states[:Pennsylvania]
